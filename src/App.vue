79f094a5f7468d50fa4fb971551550fae6e47ae1
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-08-14 13:17:44 +0300

    working with collections

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script setup>
 import HomepageView from './views/HomepageView.vue'
 
-const isDesktop = window.innerWidth > 1024
-const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
+const fruits = ['kiwi', 'banana', 'orange']
+
+const fruitsMap = {
+  kiwi: 'Super Kiwi',
+  banana: 'Super Banana',
+  orange: 'Super Orange'
+}
 </script>
 
 <template>
-  <template v-if="isDesktop">
-    <HomepageView />
+  <div :key="fruit" v-for="(fruit, index) in fruits">{{ index }}: {{ fruit }}</div>
+
+  <div :key="fruit" v-for="(fruit, key) in fruitsMap">{{ key }}: {{ fruit }}</div>
+
+  <template v-if="555">
+    <div :key="val" v-for="val in 5">{{ val }}</div>
   </template>
-  <template v-else-if="isTablet">
-    <div>this is tablet</div>
-  </template>
-  <template v-else>
-    <div>this is mobile</div>
-  </template>
+
+  <div :key="char" v-for="char in 'hello'">{{ char }}</div>
+
+  <HomepageView />
 </template>
