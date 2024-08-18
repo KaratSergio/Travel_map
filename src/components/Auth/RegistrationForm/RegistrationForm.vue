28a0fbc157bf28b45ddb577bac3a44a348b1a8e9
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-08-18 14:06:27 +0300

    v-modal

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -1,18 +1,26 @@
 <script setup>
 import IButton from '@/components/IButton/IButton.vue'
 import IInput from '@/components/IInput/IInput.vue'
+import { reactive } from 'vue'
 
 const emit = defineEmits(['submit'])
+const usserData = reactive({
+  name: '',
+  email: '',
+  password: ''
+})
 </script>
 
 <template>
-  <form
-    @submit.prevent="emit('submit', { name: 'Name' })"
-    class="max-w-[500px] bg-white p-10 rounded-2xl"
-  >
-    <IInput class="mb-4" label="Повне ім'я" />
-    <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" />
-    <IInput label="Пароль" type="password" />
+  <form @submit.prevent="emit('submit', usserData)" class="max-w-[500px] bg-white p-10 rounded-2xl">
+    <IInput class="mb-4" label="Повне ім'я" v-modal="usserData.name" />
+    <IInput
+      class="mb-4"
+      label="Електронна пошта"
+      placeholder="email@gmail.com"
+      v-modal="usserData.email"
+    />
+    <IInput label="Пароль" type="password" v-modal="usserData.password" />
     <IButton class="mt-10 w-full" variant="gradient" type="submit">Створити аккаунт</IButton>
   </form>
 </template>
--- src/components/IInput/IInput.vue
+++ src/components/IInput/IInput.vue
@@ -1,11 +1,13 @@
 <script setup>
 const props = defineProps({
+  modalValue: String,
   label: String,
   placeholder: String,
   type: { default: 'text', type: String }
 })
 
 defineOptions({ inheritAttrs: false })
+const emit = defineEmits(['update:modalValue'])
 </script>
 
 <template>
@@ -13,8 +15,9 @@ defineOptions({ inheritAttrs: false })
     <label class="block">
       <span class="block text-xs px-3 mb-2">{{ props.label }}</span>
       <input
-        @input="changeValue"
+        @input="emit('update:modalValue', $event.target.value)"
         v-bind="{ ...$props, ...$attrs }"
+        :value="modalValue"
         class="w-full text-sm rounded-[4px] border-[#eaeaea] border py-2 px-3 focus:outline-primary"
       />
     </label>
