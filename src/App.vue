264210ba9e1eb928299dacc6360f2f26a6871fa7
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-08-23 13:55:44 +0300

    Map Markers

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup>
-import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { mapSettings } from './map/settings'
-import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import MarkerIcon from './components/icons/MerkerIcon.vue'
+
+const favoritePlaces = [
+  {
+    id: 1,
+    title: 'New place 1',
+    description: 'Super description 1',
+    img: '',
+    lngLat: [30.523333, 50.490001]
+  },
+  {
+    id: 2,
+    title: 'New place 2',
+    description: 'Super description 2',
+    img: '',
+    lngLat: [30.523333, 50.450001]
+  }
+]
 </script>
 
 <template>
   <main class="flex h-screen">
-    <div class="bg-white h-full w-[480px] shrink-0 overflow-auto pb-10">
-      <FavoritePlaces />
+    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
+      <FavoritePlaces :items="favoritePlaces" />
     </div>
     <div class="w-full h-full flex items-center justify-center text-6xl">
       <MapboxMap
@@ -17,7 +35,11 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
         :zoom="10"
         :access-token="mapSettings.apiToken"
         :map-style="mapSettings.style"
-      ></MapboxMap>
+      >
+        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
+          <MarkerIcon class="h-8 w-8" />
+        </MapboxMarker>
+      </MapboxMap>
     </div>
   </main>
 </template>
