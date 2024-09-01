70693326be903440a4b39b39cb9f4de2c4e9aac4
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-09-01 11:49:47 +0300

    login

--- src/components/api/user/index.ts
+++ src/components/api/user/index.ts
@@ -3,7 +3,7 @@ import { clientFetch } from '../clientFetch'
 type RegisterBody = { name: string; password: string; email: string }
 type LoginBody = { email: string; password: string }
 
-export const login = (body: LoginBody) => clientFetch.post('/user/login', body)
+export const loginUser = (body: LoginBody) => clientFetch.post('/user/login', body)
 export const registerUser = (body: RegisterBody) => clientFetch.post('/user/register', body)
 export const logout = () => clientFetch.get('/user/logout')
 export const refresh = () => clientFetch.post('/user/refresh')
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,22 @@
 <script setup>
 import LoginForm from '@/components/Auth/LoginForm/LoginForm.vue'
+import { loginUser } from '@/components/api/user'
+import { useRouter } from 'vue-router'
+
+const isLoading = ref(false)
+const router = useRouter
+
+const hendleLogin = async (userData) => {
+  isLoading.value = true
+  try {
+    await loginUser(userData)
+    router.replace('./map')
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 
 <template>
-  <LoginForm />
+  <LoginForm @submit="hendleLogin" :is-loading="isLoading" />
 </template>
