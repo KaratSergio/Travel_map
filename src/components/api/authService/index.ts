ca80d80860c927f3fc145cb04ab9d2cb4c09c461
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-09-04 13:08:21 +0300

    refresh token

--- src/components/api/authService/index.ts
+++ src/components/api/authService/index.ts
@@ -1,4 +1,6 @@
 import { clientFetch } from '../clientFetch'
+import { router } from '../../../router'
+import type { AxiosResponse, AxiosRequestHeaders } from 'axios'
 
 export const TOKEN_KEY = import.meta.env.VITE_APP_TOKEN_KEY
 
@@ -13,30 +15,28 @@ class AuthService {
     return Boolean(this.#token)
   }
 
+  getToken(): string | null {
+    return this.#token
+  }
+
   setToken(token: string): void {
     localStorage.setItem(TOKEN_KEY, token)
-    clientFetch.defaults.headers.common = { Authorization: `Bearer ${token}` }
     this.#token = token
   }
 
   clearToken(): void {
     this.#token = null
     localStorage.removeItem(TOKEN_KEY)
-    clientFetch.defaults.headers.common = {}
   }
 
   async loginUser(body: LoginBody): Promise<void> {
-    const { data } = await clientFetch.post<TokenResponse>('/user/login', body)
-    const { accessToken } = data
-
-    this.setToken(accessToken)
+    const { data }: AxiosResponse<TokenResponse> = await clientFetch.post('/user/login', body)
+    this.setToken(data.accessToken)
   }
 
   async registerUser(body: RegisterBody): Promise<void> {
-    const { data } = await clientFetch.post<TokenResponse>('/user/register', body)
-    const { accessToken } = data
-
-    this.setToken(accessToken)
+    const { data }: AxiosResponse<TokenResponse> = await clientFetch.post('/user/register', body)
+    this.setToken(data.accessToken)
   }
 
   async logout(): Promise<void> {
@@ -45,8 +45,36 @@ class AuthService {
   }
 
   async refresh(): Promise<void> {
-    return clientFetch.get('/user/refresh')
+    const { data }: AxiosResponse<TokenResponse> = await clientFetch.get('/user/refresh')
+    this.setToken(data.accessToken)
   }
 }
 
 export const authService = new AuthService()
+
+clientFetch.interceptors.request.use((request) => {
+  const token = authService.getToken()
+
+  if (token && request.headers) {
+    ;(request.headers as AxiosRequestHeaders).Authorization = `Bearer ${token}`
+  }
+
+  return request
+})
+
+clientFetch.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        await authService.refresh()
+        return clientFetch.request(error.config)
+      } catch (e) {
+        router.push('/auth/login')
+        return Promise.reject(e)
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
